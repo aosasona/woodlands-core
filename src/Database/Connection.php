1b9843ae6a9a1f9f562d2dddc269782ed2eadafa
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-29 18:31:45 +0100

    dev: moved database stuff here

---
+++ src/Database/Connection.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Woodlands\Core\Database;
+
+use InvalidArgumentException;
+use PDO;
+use PDOStatement;
+
+class Connection
+{
+    private static ?self $instance = null;
+    private PDO $connection;
+
+    private function __construct(string $dsn, string $username, string $password)
+    {
+        $this->connection = new PDO($dsn, $username, $password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+
+    public static function getInstance(string $dsn = "", string $username = "", string $password = ""): self
+    {
+        // Automatically load database credentials from the environment if they are not provided
+        $dsn = $dsn === "" ? $_ENV["DSN"] ?? "" : $dsn;
+        $username = $username === "" ? $_ENV["DB_USER"] ?? "" : $username;
+        $password = $password === "" ? $_ENV["DB_PASSWORD"] ?? "" : $password;
+
+        if ($dsn === "") {
+            throw new InvalidArgumentException("DSN cannot be empty");
+        }
+
+        if (self::$instance == null) {
+            self::$instance = new self($dsn, $username, $password);
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getTables(): array
+    {
+        $driver = $this->connection->getAttribute(PDO::ATTR_DRIVER_NAME);
+        $sql =  "SELECT table_name FROM information_schema.tables WHERE table_schema = 'woodlands'";
+        $stmt = $this->connection->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    /**
+     * @param array<int,mixed> $params
+     */
+    public function query(string $sql, array $params = []): ?PDOStatement
+    {
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute($params);
+        return $stmt ?: null;
+    }
+}
---
+++ src/Error/Handler.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Woodlands\Core\Error;
+
+use ErrorException;
+use Throwable;
+
+class Handler
+{
+    public static function handleError(mixed $errno, mixed $errstr, mixed $errfile, mixed $errline): void
+    {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
+    public static function handleException(\Throwable $e): void
+    {
+        throw $e;
+    }
+
+    private static function getLines(Throwable $e): string
+    {
+        $file = file_get_contents($e->getFile());
+        $lines = explode("\n", $file);
+        $start = $e->getLine() - 3;
+        $start = $start < 0 ? 0 : $start;
+        $length = 5;
+        if ($start + $length > count($lines)) {
+            $length = count($lines) - $start;
+        }
+
+        $lines = array_slice($lines, $start, $length);
+        $line_numbers = range($start + 1, $start + $length);
+
+        $err_script_section = "";
+        for ($i = 0; $i < count($lines); $i++) {
+            $line_num = $line_numbers[$i] == $e->getLine() ? "--> {$line_numbers[$i]}" : $line_numbers[$i];
+            $err_script_section .= "<span>{$line_num}</span> {$lines[$i]}\n";
+        }
+
+        return $err_script_section;
+    }
+
+    public static function displayError(\Throwable $e): void
+    {
+        $err_script_section = self::getLines($e);
+
+        echo <<<HTML
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/php.min.js"></script>
+        <script>hljs.highlightAll();</script>
+      </head>
+      <style>
+      :root {
+        --highlight-color: #ef4444;
+        --border-color: #2c2c2c;
+      }
+
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      .container {
+        color: #ffffff;
+        background-color: #1a1a1a;
+        font-family: Arial, sans-serif;
+        width: 100%;
+        min-height: 100dvh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-block: 3rem;
+      }
+
+      .error_boundary {
+        width: 100%;
+        max-width: 720px;
+      }
+
+      .error_message {
+        font-size: 1.25rem;
+        font-weight: bold;
+        text-align: center;
+        background-color: var(--highlight-color);
+        color: #ffffff;
+        padding: 1rem 1.5rem;
+      }
+
+      .error_stack {
+        background-color: #181818;
+        border: 1px solid var(--border-color);
+        margin-top: 1rem;
+      }
+
+      .error_stack p {
+        background-color: var(--border-color);
+        font-size: 0.875rem;
+        padding: 0.9rem 1.5rem;
+      }
+
+      .error_stack pre {
+        padding: 1rem 1.5rem;
+        white-space: pre-wrap;
+        word-wrap: break-word;
+        line-height: 1.5;
+      }
+
+      .highlight {
+        color: var(--highlight-color);
+      }
+      </style>
+
+      <div class="container">
+        <div class="error_boundary">
+          <div class="error_message">{$e->getMessage()}</div>
+          <div class="error_stack">
+            <p>Error in <b class="highlight">{$e->getFile()}</b> on line <b class="highlight">{$e->getLine()}</b></p>
+            <pre> <code class="language-php">{$err_script_section}</code></pre>
+            <pre>{$e->getTraceAsString()}</pre>
+          </div>
+        </div>
+      </div>
+    HTML;
+
+        exit;
+    }
+}
