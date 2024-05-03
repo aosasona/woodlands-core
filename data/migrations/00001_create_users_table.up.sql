6188a7592d2a4edc0cd27b79034b69c5beadba26
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-05-03 23:30:37 +0100

    Added constraints to existing migration files

--- data/migrations/00001_create_users_table.up.sql
+++ data/migrations/00001_create_users_table.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
   `hashed_password` varchar(255) NOT NULL,
   `user_type` ENUM ('staff', 'student') NOT NULL,
   `last_signed_in_at` timestamp,
-  `created_at` timestamp DEFAULT (now()),
-  `last_modified_at` timestamp DEFAULT (now())
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `last_modified_at` timestamp NOT NULL DEFAULT (now())
 );
--- data/migrations/00002_create_students_table.up.sql
+++ data/migrations/00002_create_students_table.up.sql
@@ -1,13 +1,13 @@
 -- A lot of overlap with the staff table but keeping them separate for future extensibility
 CREATE TABLE IF NOT EXISTS students (
   `student_id` int PRIMARY KEY AUTO_INCREMENT,
-  `first_name` varchar(64),
-  `last_name` varchar(64),
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(64) NOT NULL,
   `date_of_birth` date NOT NULL,
   `gender` enum('M', 'F') NOT NULL,
-  `user_id` int NOT NULL,
-  `created_at` timestamp DEFAULT (now()),
-  `last_modified_at` timestamp DEFAULT (now()),
+  `user_id` int UNIQUE NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `last_modified_at` timestamp NOT NULL DEFAULT (now()),
 
   FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
 );
