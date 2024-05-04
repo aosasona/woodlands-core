1119623ab23723e4635cb18e586673c4c326ba61
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-05-05 00:40:14 +0100

    Updated DBML

--- data/migrations/00000_create_departments.up.sql
+++ data/migrations/00000_create_departments.up.sql
@@ -1,2 +1,7 @@
 CREATE TABLE `departments` (
+  `department_id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) UNIQUE NOT NULL,
+  `description` text,
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `last_modified_at` timestamp DEFAULT (now())
 )
