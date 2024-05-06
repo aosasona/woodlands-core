c406c9789157524df3f6341514daa237bfa531aa
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-05-06 23:01:14 +0100

    Added auth data validation

--- src/Auth.php
+++ src/Auth.php
@@ -9,12 +9,35 @@ use Woodlands\Core\Models\User;
 
 final class Auth
 {
+    /*
+     * - At least 8 characters
+     * - At least one uppercase letter
+     * - At least one lowercase letter
+     * - At least one number
+    */
+    private const PASSWORD_REGEX = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/";
+
     /**
      * @param array<int,\Woodlands\Core\Models\UserType> $allowed
      */
     public static function login(string $email, string $password, bool $remember, array $allowed): User
     {
+        // Validate the email and password
+        if(empty($email) || empty($password)) {
+            throw new AppException("Both email and password are required", 400);
+        }
+
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if($email === false || !(str_ends_with($email, "@woodlands.ac.uk"))) {
+            throw new AppException("Invalid email provided, must be in the format of an @woodlands.ac.uk email", 400);
+        }
+
+        if (empty($password) || strlen($password) < 6) {
+            throw new AppException("Password is required and must be at least 6 characters", 400);
+        }
+
         /** @var ?User $user **/
+
         $user = User::new()->where("email", "=", $email)->one();
         if(empty($user)) {
             throw new AppException("User not found", 400);
